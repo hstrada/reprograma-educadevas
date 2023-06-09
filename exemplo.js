1bb4bf74c4bb4b76fe781b74f827dae57269c034
hstrada <6079947+hstrada@users.noreply.github.com>
2023-06-09 11:47:04 -0300

    adicionando exemplo

---
+++ exemplo.js
@@ -0,0 +1,8 @@
+const categorias = ['Tênis', 'Camisetas'];
+console.log(categorias);
+
+const categoriasObj = [
+  { id: 1, nome: 'Tênis' },
+  { id: 2, nome: 'Camisetas' }
+];
+console.log(categoriasObj[0].nome);
